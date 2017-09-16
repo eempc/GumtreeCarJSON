d547e824e44400246aa65470fc39a90eb152feb6
Evie <evc484@gmail.com>
2017-09-16 18:37:44 +0100

    Initial commit

---
+++ GumtreeCarJSON/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace GumtreeCarJSON {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(701, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 529);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(701, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openFileToolStripMenuItem.Text = "Open file";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(701, 505);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(693, 479);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(693, 479);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Log";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(693, 479);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Empty";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 551);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Gumtree HTML text extraction";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GumtreeCarJSON/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using HtmlAgilityPack;
+using ClosedXML.Excel;
+
+namespace GumtreeCarJSON {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            InitFBD();
+            InitString();
+        }
+
+        OrderedDictionary currentCar = new OrderedDictionary();
+        string dataLayerStart, dataLayerEnd;
+        string[] attributes;
+        string saveFile = @"D:\Temp\Car Database\Car v2.xlsx";
+
+        public void InitString() {
+            dataLayerStart = "var dataLayer = ";
+            dataLayerEnd = "\n<!--GTM Pt1 -- >";
+            attributes = new string[] { "vrn", "vehicle_make", "vehicle_model", "vehicle_registration_year", "seller_type", "vehicle_mileage", "vehicle_colour", "price", "vehicle_not_writeoff", "vehicle_vhc_checked", "URL", "Location", "MOT expiry" };
+            foreach (string a in attributes)
+                currentCar.Add(a, "");
+        }    
+
+        public void InitFBD() {
+            openFileDialog1.Filter = "HTML (*.html;*.htm)|*.html;*.htm|" + "All files (*.*)|*.*";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "HTML file browser";
+        }
+
+        public void GumtreeCarHTMLExtract() {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                foreach (string file in openFileDialog1.FileNames) {
+                    //Retrieve JSON
+                    string fullHTML = File.ReadAllText(file);
+                    string dataLayer = ExtractRegex(fullHTML, dataLayerStart, dataLayerEnd);
+                    dynamic obj = JsonConvert.DeserializeObject(dataLayer);
+
+                    //Unfortunately I do not know if there is a way to turn a string into the json .dot thing, if I could, then the following would be a one line loop
+                    //Maybe in a future commit
+                    currentCar[attributes[0]] = obj[0].a.attr.vrn;
+                    currentCar[attributes[1]] = obj[0].a.attr.vehicle_make;
+                    currentCar[attributes[2]] = obj[0].a.attr.vehicle_model;
+                    currentCar[attributes[3]] = obj[0].a.attr.vehicle_registration_year;
+                    currentCar[attributes[4]] = obj[0].a.attr.seller_type;
+                    currentCar[attributes[5]] = obj[0].a.attr.vehicle_mileage;
+                    currentCar[attributes[6]] = obj[0].a.attr.vehicle_colour;
+                    currentCar[attributes[7]] = obj[0].a.attr.price;
+                    currentCar[attributes[8]] = obj[0].a.attr.vehicle_not_writeoff;
+                    currentCar[attributes[9]] = obj[0].a.attr.vehicle_vhc_checked;
+
+                    //Retrieve URL and location from main source as this is a downloaded HTML file on local drive
+                    currentCar[attributes[10]] = ExtractRegex(fullHTML, "https://", " -->");
+                    currentCar[attributes[11]] = ExtractLocation(fullHTML);
+
+                    WriteToXLSX();
+                }
+            }
+        }
+
+        public void WriteToXLSX() {
+            if (!Directory.Exists(Path.GetDirectoryName(saveFile))) 
+                Directory.CreateDirectory(Path.GetDirectoryName(saveFile));
+
+            if (!File.Exists(saveFile))
+                File.Create(saveFile);
+
+            var wb = new XLWorkbook(saveFile);
+            var ws = wb.Worksheet(0);
+
+            System.Collections.ICollection vals = currentCar.Values;
+
+            int rows = ws.RowsUsed().Count();
+
+            if (rows == 0) {
+                for (int i = 0; i < vals.Count; i++) {
+                    ws.Cell(1, i).Value = attributes[0];
+                }
+                rows = ws.RowsUsed().Count();
+            }
+
+            for (int i = 0; i < vals.Count; i++)
+                ws.Cell(rows + 1, i + 1).Value = currentCar[i];
+
+            wb.SaveAs(saveFile);
+        }
+
+        public string ExtractLocation(string source) {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(source);
+            var singleNode = doc.DocumentNode.SelectSingleNode("//body/div/div/div/main/div/header/strong/span");
+            return singleNode.InnerText.ToString();
+        }
+
+        public string ExtractRegex(string source, string start, string end) {
+            Regex rx = new Regex(start + "(.*?)" + end);
+            var match = rx.Match(source);
+            return match.Groups[1].ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            GumtreeCarHTMLExtract();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+    }
+}
